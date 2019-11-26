f9e09c36e1f43ad21be43b64d2c7bbb67f21a89d
milakoliseva <koliseva.mila@gmail.com>
2019-11-26 09:16:56 +0100

    graphql iso validate time

--- dist/utils/validator.js
+++ dist/utils/validator.js
@@ -50,7 +50,7 @@ var leapYear = function leapYear(year) {
 // - Leap seconds cannot be known in advance.
 //
 var validateTime = exports.validateTime = function validateTime(time) {
-  var TIME_REGEX = /^([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(\.\d{1,})?(([Z])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9]))$/;
+  var TIME_REGEX = /^([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(\.\d{1,})?(([Z])|([+|-]([01][0-9]|2[0-3]):[0-5][0-9])|())$/;
   return TIME_REGEX.test(time);
 };
 
